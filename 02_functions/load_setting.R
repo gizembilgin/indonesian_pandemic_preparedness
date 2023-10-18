da9c184a80eb4b809c03b354d658446c8251fbc3
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2023-10-18 15:42:47 +1100

    include pop loading into load_setting

--- 02_functions/load_setting.R
+++ 02_functions/load_setting.R
@@ -2,13 +2,15 @@
 
 load_setting <- function(this_setting = "Indonesia") {
   
-  
+  load("01_inputs/population_MASTER.Rdata")
   
   #CHECK inputs
-  # #if (! this_setting %in% unique()){  #English names
-  #   if (! this_setting %in% unique()){ #IDN names
-  #     stop()
-  #   }
-  # }
-  
+  if (! this_setting %in% unique(population_MASTER$name_english) || ! this_setting %in% unique(population_MASTER$name_indonesian)){  
+      stop("this_setting does not exist in population_MASTER")
+  }
+
+  pop <- population_MASTER %>%
+    filter(name_english == this_setting | name_indonesian == this_setting) %>%
+    select(age_group,individuals)
+  rm(population_MASTER)
 }
