28ce27b9aea7dc226bf5cb8bb86817f41a4e283c
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2023-11-09 14:19:25 +1100

    Update configure_inital_state.R
    allowing infection in the model!

--- 02_functions/configure_inital_state.R
+++ 02_functions/configure_inital_state.R
@@ -1,6 +1,9 @@
 
 
-configure_inital_state <- function(age_group_labels = c("0 to 4","5 to 17","18 to 29","30 to 59","60 to 110"),
+configure_inital_state <- function(introduction,
+                                   average_symptomatic_period,
+                                   average_exposed_period,
+                                   age_group_labels = c("0 to 4","5 to 17","18 to 29","30 to 59","60 to 110"),
                                    population = loaded_setting_characteristics$population,
                                    comorbidities = loaded_setting_characteristics$comorbidities) {
   
@@ -19,21 +22,22 @@ configure_inital_state <- function(age_group_labels = c("0 to 4","5 to 17","18 t
     ) %>%
     select(-proportion)
   
-  #add in empty E, I, R classes  
-  workshop <- inital_state %>%
-    mutate(individuals = 0)
-  workshop <- crossing(class = c("E","I","R"), workshop) 
-  inital_state <- inital_state %>%
-    mutate(class = "S")
-  inital_state = rbind(inital_state,workshop)
-  
+  #include SEIR classes
+  inital_state <- crossing(class = c("S","E","I","R"), inital_state) %>%
+    mutate(individuals = case_when(
+      class == "S" ~ individuals * (1-introduction),
+      class == "E" ~ individuals * introduction * average_exposed_period/(average_exposed_period + average_symptomatic_period),
+      class == "I" ~ individuals * introduction * average_symptomatic_period/(average_exposed_period + average_symptomatic_period),
+      class == "R" ~ 0
+    ))
+
   # order correctly
   inital_state$class <- factor(inital_state$class, levels = c("S","E","I","R"))
   inital_state$age_group <- factor(inital_state$age_group, levels = age_group_labels)
   inital_state <- inital_state %>%
     arrange(class,comorbidity,vaccination_status,age_group)  
   
-  if(sum(round(inital_state$individuals)) != sum(population$individuals)){stop("inital state does not match population")}
+  if(abs(sum(round(inital_state$individuals)) - sum(population$individuals))>1){stop("inital state does not match population")}
   if(sum(inital_state$individuals[inital_state$vaccination_status != 0]) > 0 ){stop("inital state contains vaccinated individuals")}
   
   return(inital_state)
