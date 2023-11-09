09da2c96b3e3917cdfbde634bdba6ded38a20f79
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2023-11-09 14:21:02 +1100

    first run of the command_deck with infection!

--- command_deck.R
+++ command_deck.R
@@ -4,7 +4,8 @@
 ### (1/2) Setup                 
 ################################################################################
 # load libraries
-require(tidyverse)
+require(tidyverse);require(deSolve)
+options(scipen = 1000)
 
 # load all functions
 for (function_script in list.files(path="02_functions/", full.name = TRUE)){source(function_script)}
@@ -13,13 +14,22 @@ for (function_script in list.files(path="02_functions/", full.name = TRUE)){sour
 age_group_labels = c("0 to 4","5 to 17","18 to 29","30 to 59","60 to 110")
 
 TOGGLE_setting = "Indonesia"
+
+# pathogen characteristics
 TOGGLE_R0_to_fit = 2
 TOGGLE_average_symptomatic_period = 7
+TOGGLE_average_exposed_period = 7
 TOGGLE_prevalence_symptoms = rep(0.8,rep(length(age_group_labels)))
 TOGGLE_reduced_infectiousness_asymptomatic = 0.5
 TOGGLE_susceptibility = rep(0.8,rep(length(age_group_labels)))
+TOGGLE_average_immune_period = 365*10
 
+TOGGLE_NPI = 0.5
+TOGGLE_infection_derived_immunity = 1
+TOGGLE_vaccine_derived_immunity = 0.8
 
+TOGGLE_introduction_day = 0 #NB: just change length of run if later
+TOGGLE_introductions = 0.00001 #percentage of pop of introductions on day
 
 #_______________________________________________________________________________
 
@@ -27,12 +37,42 @@ TOGGLE_susceptibility = rep(0.8,rep(length(age_group_labels)))
 ### (2/2) Run model            
 ################################################################################
 loaded_setting_characteristics <- load_setting(this_setting = TOGGLE_setting)
-inital_state <- configure_inital_state()
-fit_beta_to_R0(
+
+inital_state <- configure_inital_state(
+  introduction = TOGGLE_introductions,
+  average_symptomatic_period = TOGGLE_average_symptomatic_period,
+  average_exposed_period  = TOGGLE_average_exposed_period 
+  )
+
+fitted_beta <- fit_beta_to_R0(
   R0_to_fit = TOGGLE_R0_to_fit,
   this_average_symptomatic_period = TOGGLE_average_symptomatic_period,
   this_prevalence_symptoms = TOGGLE_prevalence_symptoms,
   this_reduced_infectiousness_asymptomatic = TOGGLE_reduced_infectiousness_asymptomatic,
   this_susceptibility = TOGGLE_susceptibility
 )
+
+parameters = list(
+  suscept = TOGGLE_susceptibility,
+  beta=fitted_beta,
+  NPI=TOGGLE_NPI,
+  contact_matrix=loaded_setting_characteristics$contact_matrix,
+  lota=TOGGLE_reduced_infectiousness_asymptomatic,
+  gamma=TOGGLE_prevalence_symptoms,
+  lambda=1/TOGGLE_average_exposed_period,
+  delta=1/TOGGLE_average_symptomatic_period,
+  omega=1/TOGGLE_average_immune_period,
+  rho=TOGGLE_infection_derived_immunity,
+  age_group_labels=age_group_labels,
+  VE=TOGGLE_vaccine_derived_immunity,
+  num_age_groups=num_age_groups)
+
+incidence_log_tidy<- run_disease_model()
+
+
+to_plot <- incidence_log_tidy %>%
+  group_by(time) %>%
+  summarise(incidence = sum(incidence))
+ggplot(to_plot) + 
+  geom_point(aes(x=time,y=incidence))
 #_______________________________________________________________________________
