3ba8ed4273450a42a85771704f5295258fb5f98d
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2023-11-15 11:48:22 +1100

    rename of population_by_risk_group to _by_comorbidity

--- 02_functions/configure_inital_state.R
+++ 02_functions/configure_inital_state.R
@@ -4,11 +4,11 @@ configure_inital_state <- function(introduction,
                                    average_symptomatic_period,
                                    average_exposed_period,
                                    age_group_labels = c("0 to 4","5 to 17","18 to 29","30 to 59","60 to 110"),
-                                   population_by_risk_group = loaded_setting_characteristics$population_by_risk_group,
+                                   population_by_comorbidity = loaded_setting_characteristics$population_by_comorbidity,
                                    population = loaded_setting_characteristics$population,
                                    comorbidities = loaded_setting_characteristics$comorbidities) {
   
-  inital_state <- crossing(population_by_risk_group,
+  inital_state <- crossing(population_by_comorbidity,
                                        vaccination_status = c(0,1)) %>%
     mutate(individuals =
              case_when(vaccination_status != 0 ~ 0, # assume no one vaccinated at the initial time point
