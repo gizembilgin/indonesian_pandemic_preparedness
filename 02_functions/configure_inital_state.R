7b3c6f3ef10277aa04881ddb6cec14bc4a246b6c
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2024-01-15 13:43:08 +1100

    tidy code in configure_inital_state

--- 02_functions/configure_inital_state.R
+++ 02_functions/configure_inital_state.R
@@ -4,24 +4,22 @@ configure_inital_state <- function(introduction,
                                    average_symptomatic_period,
                                    average_exposed_period,
                                    age_group_labels = c("0 to 4","5 to 17","18 to 29","30 to 59","60 to 110"),
-                                   population_by_comorbidity = loaded_setting_characteristics$population_by_comorbidity,
-                                   population = loaded_setting_characteristics$population,
-                                   comorbidities = loaded_setting_characteristics$comorbidities) {
+                                   population_by_comorbidity = loaded_setting_characteristics$population_by_comorbidity) {
   
   inital_state <- crossing(population_by_comorbidity,
-                                       vaccination_status = c(0,1)) %>%
+                           vaccination_status = c(0, 1)) %>%
     mutate(individuals =
-             case_when(vaccination_status != 0 ~ 0, # assume no one vaccinated at the initial time point
+             case_when(vaccination_status != 0 ~ 0, # no one is vaccinated at the initial time point
                        TRUE ~ individuals)) 
   
-  #include SEIR classes
+  # include SEIR classes
   inital_state <- crossing(class = c("S","E","I","R","Incid"), inital_state) %>%
     mutate(individuals = case_when(
       class == "S" ~ individuals * (1-introduction),
       class == "E" ~ individuals * introduction * average_exposed_period/(average_exposed_period + average_symptomatic_period),
       class == "I" ~ individuals * introduction * average_symptomatic_period/(average_exposed_period + average_symptomatic_period),
       class == "R" ~ 0,
-      class == "Incid" ~ 0 #add empty rows for incidence tracker
+      class == "Incid" ~ 0 # empty rows for incidence tracker
     ))
 
   # order correctly
@@ -30,8 +28,8 @@ configure_inital_state <- function(introduction,
   inital_state <- inital_state %>%
     arrange(class,comorbidity,vaccination_status,age_group)  
   
-  if(abs(sum(round(inital_state$individuals)) - sum(population$individuals))>1){stop("inital state does not match population")}
-  if(sum(inital_state$individuals[inital_state$vaccination_status != 0]) > 0 ){stop("inital state contains vaccinated individuals")}
+  if(abs(sum(round(inital_state$individuals)) - sum(population_by_comorbidity$individuals))>1) stop("configure_inital_state: inital state does not match population")
+  if(sum(inital_state$individuals[inital_state$vaccination_status != 0]) > 0 ) stop("configure_inital_state: inital state contains vaccinated individuals")
   
   return(inital_state)
 }
