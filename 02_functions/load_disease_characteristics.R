bd16fd78b26f7ee93eff79d40f8c1969100285e7
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2024-01-15 12:22:40 +1100

    comment in load_disease_characteristics
    blank file

--- 02_functions/load_disease_characteristics.R
+++ 02_functions/load_disease_characteristics.R
@@ -1,4 +1,5 @@
 
+#NB: not currently used, but may be of use when severe outcomes are included, or characteristics of disease limited a bit further
 
 load_disease_characteristics <- function() {
   
