bd5c677bde4197d4e883f8f8d44fb53696dc779e
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2024-01-15 12:01:22 +1100

    Update workshop - troubleshooting.R

--- workshop - troubleshooting.R
+++ workshop - troubleshooting.R
@@ -1,32 +1,31 @@
 
 ### 11/01/2024 model doesn't run when vaccine_delivery_start_date = 50
 
-# next_state[round(next_state$individuals)<0,]
+next_state[round(next_state$individuals)<0,]
 # class age_group comorbidity vaccination_status individuals
-# 15     S 60 to 110           1                  0   -3088.644
-# 75     R 60 to 110           1                  0   -2799.546
+# 15     S 60 to 110           1                  0  -27648.827
+# 35     E 60 to 110           1                  0   -1117.090
+# 55     I 60 to 110           1                  0   -1262.656
+# 75     R 60 to 110           1                  0  -12635.446
 
-# > todays_vaccinations
+todays_vaccinations
 # age_group comorbidity doses_delivered
-#   1 18 to 29            0          65926.
-# 2 18 to 29            1          46393.
-# 3 30 to 59            0          89304.
-# 4 30 to 59            1         141398.
-# 5 60 to 110           0          18653.
-# 6 60 to 110           1          78759.
+# 1 18 to 29            0          49075.
+# 2 18 to 29            1          34534.
+# 3 30 to 59            0          66477.
+# 4 30 to 59            1         105254.
+# 5 60 to 110           0          35443.
+# 6 60 to 110           1         149650.
 
-# this_phase
+this_phase
 # [1] "older adults followed by all adults"
-# > unique(vaccination_history$phase)
+unique(vaccination_history$phase)
 # [1] "essential workers"                   "older adults followed by all adults" "all adults at the same time"        
 # [4] "children before adults"   
 #i.e., first vaccination strategy
 
-# this_time
+this_time
 # [1] 125
-# min(vaccination_history$time[vaccination_history$phase != "essential workers"])
-# [1] 97
-#i.e., much after delivery to essential workers
 
 vaccination_history %>%
   filter(phase %in% c(this_phase,"essential workers") & #include essential workers always to capture day of concurrent delivery with others
@@ -54,15 +53,41 @@ vaccination_history %>%
   summarise(doses_delivered = sum(doses_delivered))
 # age_group comorbidity doses_delivered
 # <chr>           <dbl>           <dbl>
-#   1 60 to 110           0         527303.
-# 2 60 to 110           1        2226392.
+# 1 60 to 110           0         2703208
+# 2 60 to 110           1        11413544
 
 next_state %>% 
   filter(age_group == "60 to 110") %>%
   group_by(comorbidity,vaccination_status) %>%
   summarise(individuals = sum(individuals))
 # comorbidity vaccination_status individuals
-# 1           0                  0     281152.
-# 2           0                  1    5087232.
-# 3           1                  0      -5888.
-# 4           1                  1   21479426.
+# 1           0                  0     272442.
+# 2           0                  1    5095942.
+# 3           1                  0     -42664.
+# 4           1                  1   21516202.
+
+
+
+vaccination_history %>%
+  filter(phase %in% c("essential workers")) %>%
+  group_by(age_group,comorbidity) %>%
+  summarise(doses_delivered = sum(doses_delivered))
+vaccination_history %>%
+  filter(phase == this_phase & supply == this_supply & time <= this_time) %>%
+  group_by(age_group,comorbidity) %>%
+  summarise(doses_delivered = sum(doses_delivered))
+next_state %>% 
+  filter(vaccination_status == 1 &
+           individuals != 0) %>%
+  group_by(age_group,comorbidity) %>%
+  summarise(individuals = sum(individuals), .groups = "keep")
+
+check = vaccination_history %>% filter(phase == this_phase) %>% pivot_wider(values_from = doses_delivered, names_from = supply)
+#NB: cascade not working because doses delivered per day after essential workers NOT the same across different vaccine supplies, even with fixed rollout capacity???? go back into configure_vaccination_history
+
+check = vaccination_history %>%
+  filter(phase == this_phase) %>%
+  group_by(time,supply) %>%
+  summarise(doses_delivered = sum(doses_delivered))%>% 
+  pivot_wider(values_from = doses_delivered, names_from = supply)
+#but daily doses delivered the same!!!  
