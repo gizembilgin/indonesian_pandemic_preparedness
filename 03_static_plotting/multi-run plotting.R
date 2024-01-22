9ce675b8a6fef3c703209521fbded03c1f7992b7
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2024-01-22 14:37:44 +1100

    configure cumulative plot

--- 03_static_plotting/multi-run plotting.R
+++ 03_static_plotting/multi-run plotting.R
@@ -26,19 +26,19 @@ multiscenario_facet_plot <- function(data,
                                      yaxis_title,
                                      include_cascade = 0){
   
-  if ("flag_reconstructed" %in% colnames(data) & include_cascade == 0){
-    to_plot <- data %>% filter(flag_reconstructed == 0)
-  } else if ("flag_reconstructed" %in% colnames(data) & include_cascade == 1){
-    to_plot <- data %>%
-      filter(! phase %in% c("essential workers"))
-  }
-  
-  
   this_configuration <- default_configuration[! names(default_configuration) %in% {{this_var}}]
-  to_plot <-  configuration_filter(to_plot,this_configuration)
+  to_plot <-  configuration_filter(data,this_configuration)
     
   
   if (yaxis_title == "incidence"){
+    
+    if (include_cascade == 0){
+      to_plot <- to_plot %>% 
+        filter(flag_reconstructed == 0)
+    } else if (include_cascade == 1){
+      to_plot <- to_plot %>%
+        filter(! phase %in% c("essential workers"))
+    }
     
     ggplot(to_plot) +
       geom_line(aes(x=time,y=incidence,color=as.factor(phase)),linewidth = 1.25)  +
@@ -52,7 +52,38 @@ multiscenario_facet_plot <- function(data,
     
     to_plot <- to_plot %>%
       group_by(phase,supply,setting,vaccine_delivery_start_date,R0,infection_derived_immunity,rollout_modifier,vaccine_derived_immunity) %>%
+      arrange(time) %>%
       mutate(cumulative_incidence = cumsum(incidence))
+    
+    if (include_cascade == 0){
+      #remove strategy before essential workers
+      max_essential_workers_time = to_plot %>%
+        filter(phase == "essential workers") %>%
+        summarise(max_time = max(time), .groups = "keep")%>%
+        ungroup() %>%
+        select(-phase,-supply)
+      cumulative_no_vax = to_plot %>%
+        filter(time == vaccine_delivery_start_date - 1 & 
+                 phase == "no vaccine") %>%
+        summarise(cum_no_vax = cumulative_incidence, .groups = "keep") %>%
+        ungroup() %>%
+        select(-phase,-supply)
+        
+      to_plot <- to_plot %>% 
+        left_join(max_essential_workers_time, by = join_by(setting, vaccine_delivery_start_date,
+                                                           R0, infection_derived_immunity, rollout_modifier, vaccine_derived_immunity)) %>%
+        left_join(cumulative_no_vax, by = join_by(setting, vaccine_delivery_start_date,
+                                                  R0, infection_derived_immunity, rollout_modifier, vaccine_derived_immunity)) %>%
+        filter(!(time <= max_time & ! phase %in% c("no vaccine","essential workers"))) %>%
+        mutate(cumulative_incidence = case_when(
+          phase == "essential workers" ~ cumulative_incidence + cum_no_vax,
+          TRUE ~ cumulative_incidence
+        ))
+      
+    } else if (include_cascade == 1){
+      to_plot <- to_plot %>%
+        filter(! phase %in% c("essential workers"))
+    }
     
     ggplot(to_plot) + 
       geom_line(aes(x=time,y=cumulative_incidence,color=as.factor(phase)),linewidth = 1.25)  +
@@ -71,13 +102,18 @@ multiscenario_facet_plot <- function(data,
 
 multiscenario_facet_plot(ship_log_completed,"R0","incidence")
 multiscenario_facet_plot(ship_log_completed,"vaccine_delivery_start_date","incidence")
-multiscenario_facet_plot(ship_log_completed,"vaccine_delivery_start_date","incidence")
 #multiscenario_facet_plot(ship_log,"supply","incidence")
 multiscenario_facet_plot(ship_log_completed,"infection_derived_immunity","incidence")
 multiscenario_facet_plot(ship_log_completed,"rollout_modifier","incidence")
 multiscenario_facet_plot(ship_log_completed,"vaccine_derived_immunity","incidence")
 
-
+multiscenario_facet_plot(ship_log_completed,"R0","cumulative_incidence")
+multiscenario_facet_plot(ship_log_completed,"vaccine_delivery_start_date","cumulative_incidence")
+#multiscenario_facet_plot(ship_log,"supply","cumulative_incidence")
+multiscenario_facet_plot(ship_log_completed,"infection_derived_immunity","cumulative_incidence")
+multiscenario_facet_plot(ship_log_completed,"rollout_modifier","cumulative_incidence")
+multiscenario_facet_plot(ship_log_completed,"vaccine_derived_immunity","cumulative_incidence")
+#NB: caution this only is plotting the phase of delivery you are in at this cumulative incidence level, NOT NECCESSARY the difference in the effect of vaccines to essential workers vs the general public
 
 
 
