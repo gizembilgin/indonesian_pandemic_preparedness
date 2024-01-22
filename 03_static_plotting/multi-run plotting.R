a8fcfe49daa786db8d3d4e8986c73620680a1a4a
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2024-01-22 13:51:18 +1100

    make all multiscenario_plots plot ship_log_completed

--- 03_static_plotting/multi-run plotting.R
+++ 03_static_plotting/multi-run plotting.R
@@ -26,9 +26,6 @@ multiscenario_facet_plot <- function(data,
                                      yaxis_title,
                                      include_cascade = 0){
   
-  to_plot <- data %>%
-    group_by(time,phase,supply,setting,vaccine_delivery_start_date,R0,infection_derived_immunity,rollout_modifier,vaccine_derived_immunity) %>%
-    summarise(incidence = sum(incidence), .groups = "keep") 
   if ("flag_reconstructed" %in% colnames(data) & include_cascade == 0){
     to_plot <- data %>% filter(flag_reconstructed == 0)
   } else if ("flag_reconstructed" %in% colnames(data) & include_cascade == 1){
@@ -57,59 +54,28 @@ multiscenario_facet_plot <- function(data,
       group_by(phase,supply,setting,vaccine_delivery_start_date,R0,infection_derived_immunity,rollout_modifier,vaccine_derived_immunity) %>%
       mutate(cumulative_incidence = cumsum(incidence))
     
-    #add no vax period and essential workers to all vaccination strategies
-    no_vax_cumulative <- to_plot %>%
-      filter(phase == "no vaccine" &
-               time == vaccine_delivery_start_date - 1) %>%
-      ungroup() %>%
-      select(-time,-phase,-supply,-incidence) %>%
-      rename(cumulative_no_vax = cumulative_incidence)
-    
-    end_of_essential_workers_phase <- to_plot %>%
-      filter(phase == "essential workers") %>%
-      summarise(target_time = max(time), .groups = "keep") %>%
-      ungroup() %>%
-      select(setting,vaccine_delivery_start_date,R0,infection_derived_immunity,rollout_modifier,vaccine_derived_immunity,target_time)
-    essential_workers_cumulative <- to_plot %>%
-      filter(phase == "essential workers")  %>%
-      left_join(end_of_essential_workers_phase, by = c("setting","vaccine_delivery_start_date","R0","infection_derived_immunity","rollout_modifier","vaccine_derived_immunity")) %>%
-      filter(time == target_time) %>%
-      ungroup() %>%
-      select(-time,-phase,-supply,-incidence,-target_time) %>%
-      rename(cumulative_essential_workers = cumulative_incidence)
-      
-    to_plot <- to_plot %>%
-      left_join(no_vax_cumulative, by = join_by(setting, vaccine_delivery_start_date, R0, infection_derived_immunity, rollout_modifier, vaccine_derived_immunity)) %>%
-      left_join(essential_workers_cumulative, by = join_by(setting, vaccine_delivery_start_date, R0, infection_derived_immunity, rollout_modifier, vaccine_derived_immunity)) %>%
-      mutate(cumulative_incidence = case_when(
-        phase == "no vaccine" ~ cumulative_incidence,
-        phase == "essential workers" ~ cumulative_incidence + cumulative_no_vax,
-        TRUE ~ cumulative_incidence + cumulative_no_vax + cumulative_essential_workers)
-      )
-    
-
-    
     ggplot(to_plot) + 
-      geom_line(aes(x=time,y=cumulative_incidence,color=as.factor(phase),linetype = as.factor(supply)),linewidth = 1.25)  +
+      geom_line(aes(x=time,y=cumulative_incidence,color=as.factor(phase)),linewidth = 1.25)  +
       labs(color="", linetype = "")+
       ylab("cumulative incidence") + 
-      guides(color = guide_legend(nrow = 2))
+      guides(color = guide_legend(nrow = 2)) +
+      theme(legend.position="bottom") +
+      facet_grid(.data[[this_var]] ~.)+
+      labs(title = this_var)
+    
 
   }
 
 }
 
-multiscenario_facet_plot(ship_log,"R0","incidence")
+
 multiscenario_facet_plot(ship_log_completed,"R0","incidence")
-
-multiscenario_facet_plot(ship_log,"vaccine_delivery_start_date","incidence")
 multiscenario_facet_plot(ship_log_completed,"vaccine_delivery_start_date","incidence")
-multiscenario_facet_plot(ship_log_completed,"vaccine_delivery_start_date","incidence",include_cascade = 1)
-
+multiscenario_facet_plot(ship_log_completed,"vaccine_delivery_start_date","incidence")
 #multiscenario_facet_plot(ship_log,"supply","incidence")
-multiscenario_facet_plot(ship_log,"infection_derived_immunity","incidence")
-multiscenario_facet_plot(ship_log,"rollout_modifier","incidence")
-multiscenario_facet_plot(ship_log,"vaccine_derived_immunity","incidence")
+multiscenario_facet_plot(ship_log_completed,"infection_derived_immunity","incidence")
+multiscenario_facet_plot(ship_log_completed,"rollout_modifier","incidence")
+multiscenario_facet_plot(ship_log_completed,"vaccine_derived_immunity","incidence")
 
 
 
