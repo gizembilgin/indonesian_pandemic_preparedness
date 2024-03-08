38ef3e9542fa551f66543600e72fef44e5d51c0f
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2024-03-08 15:19:04 +1100

    print warning if var_range removes all simulations

--- 02_functions/filter_scenarios.R
+++ 02_functions/filter_scenarios.R
@@ -9,6 +9,7 @@ filter_scenarios <- function(data,configuration,warning_search = 0){
       data = data %>% 
         filter(.data[[names(configuration)[[i]]]] %in% configuration[[i]]) 
     }
+    if (nrow(data) == 0) stop(paste("filter_scenarios: There are no simulations using",names(configuration)[[i]],"=",configuration[[i]]))
     if (warning_search == 1 & nrow(data[data$supply != 0,]) == 0){
       return(names(configuration)[[i]])
     }
