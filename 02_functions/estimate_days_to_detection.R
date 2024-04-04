2400e9db87823ca327dfe693047db5fa1d6a1432
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2024-04-04 15:02:28 +1100

    spacing function nicer

--- 02_functions/estimate_days_to_detection.R
+++ 02_functions/estimate_days_to_detection.R
@@ -1,10 +1,11 @@
 
-estimate_days_to_detection <- function(outcome_threshold, # threshold number of this outcome for detection
-                              gen_interval,      # generation interval (days)
-                              IR_outcome,        # incidence rate for this outcome
-                              develop_outcome,   # time to developing outcome (days)
-                              R0                 # basic reproduction number
-                              ) {
+estimate_days_to_detection <- function (
+    outcome_threshold, # threshold number of this outcome for detection
+    gen_interval,      # generation interval (days)
+    IR_outcome,        # incidence rate for this outcome
+    develop_outcome,   # time to developing outcome (days)
+    R0                 # basic reproduction number
+) {
   (log(outcome_threshold/IR_outcome, base = R0) + 1)*gen_interval + develop_outcome
 }
 
