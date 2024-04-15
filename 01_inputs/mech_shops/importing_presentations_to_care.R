340c348ab37742c85f626a24b4217e18847dac56
Gizem Bilgin <gizem.bilgin@anu.edu.au>
2024-04-15 15:15:14 +1000

    Create importing_presentations_to_care.R

---
+++ 01_inputs/mech_shops/importing_presentations_to_care.R
@@ -0,0 +1,30 @@
+load(file = paste0(gsub("indonesian_pandemic_preparedness","data",getwd()),"/x_results/presentations_to_care.Rdata"))
+load(file = "01_inputs/population_MASTER_single_age_groups.Rdata")
+
+age_group_labels <- c("0 to 4","5 to 17","18 to 29","30 to 59","60 to 110")
+age_groups_num <- c(0,4,17,29,59,110)
+underlying_age_grouping <- c(0,9,19,29,39,49,59,69,79,110)
+
+presentations_to_care <- presentations_to_care %>% rename(agegroup_RAW = age)
+
+pop_RAW =  population_MASTER_single_age_groups %>%
+  filter(name_english == "Indonesia") %>%
+  select(-name_english,-name_indonesian) %>%
+  mutate(agegroup_RAW = cut(age_group_single,breaks = underlying_age_grouping, include.lowest = T, labels = unique(presentations_to_care$agegroup_RAW)),
+         agegroup_MODEL = cut(age_group_single,breaks = age_groups_num, include.lowest = T, labels = age_group_labels)) %>%
+  ungroup() %>%
+  group_by(agegroup_MODEL) %>%
+  mutate(model_group_percent = individuals/sum(individuals)) 
+
+presentations_to_care = pop_RAW %>% 
+  left_join(presentations_to_care, by = c("agegroup_RAW")) %>% 
+  mutate(interim = model_group_percent * percentage) %>%
+  group_by(agegroup_MODEL) %>%
+  summarise(proportion = sum(interim)) %>%
+  rename(age_group = agegroup_MODEL)
+
+save(presentations_to_care, file = paste0(gsub("indonesian_pandemic_preparedness","data",getwd()),"/x_results/presentations_to_care_model_age_groups.Rdata"))
+
+ggplot(presentations_to_care) + 
+  geom_col(aes(age_group,proportion))
+rm(presentations_to_care)
